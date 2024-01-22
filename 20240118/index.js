d58e3b75618d249856a87eba7383cfe5aa6d104f
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2024-01-22 13:53:10 +0100

    20240118

---
+++ 20240118/index.js
@@ -0,0 +1,18 @@
+console.log("szia");
+
+function ki(x){
+    console.log(x);
+}
+
+ki("szia");
+
+function g(x,y){
+    return 2*x+3*y;
+}
+
+// document.body.children[1].innerHTML=5+6;
+//document.getElementById("alma").innerHTML=69;
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("alma").innerHTML=69;
+  });
